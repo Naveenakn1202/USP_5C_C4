02964fb6a7357e1d037c0e45d9bdfa1c0f9635d9
Naveena K N <84121719+Naveenakn1202@users.noreply.github.com>
2021-11-12 16:08:20 +0530

    Create P5.sh

---
+++ P5.sh
@@ -0,0 +1,18 @@
+Shell script to find the factorial of a number
+#!/bin/sh
+	
+
+	echo "Enter a Number to find the factorial:"
+	read n
+	fact=1
+	temp=$n
+	
+
+	while [ $n -gt 0 ]
+	do 
+		fact=`expr $n \* $fact`
+		n=`expr $n - 1`
+	done
+	
+
+	echo "Factorial of $temp is $fact"
