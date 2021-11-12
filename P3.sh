e30c2c80445f11d86dc01e1d2560872def677895
Naveena K N <84121719+Naveenakn1202@users.noreply.github.com>
2021-11-12 16:07:12 +0530

    Create P3.sh

---
+++ P3.sh
@@ -0,0 +1,17 @@
+Shell script to check whether the number is zero/ positive/ negative
+#!/bin/sh
+	
+
+	echo "Enter a number"
+	read n
+	
+
+	if [ $n -lt 0 ]
+	then
+		echo "Negative Number"
+	elif [ $n -eq 0 ]
+	then
+		echo "Zero "
+	else 
+		echo "Postive Number"
+	fi
